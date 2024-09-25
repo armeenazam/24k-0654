eb4c6a4556104bb1163dcf96ec5640ed5e9ba5e6
armeenazam <ayanazam1976@gmail.com>
2024-09-25 21:18:51 +0500

    Add files via upload

---
+++ lab5/q7.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int num;
+	printf("Enter a number: ");
+	scanf("%d",&num);
+	(num%10==0)?printf("Last digit is zero"):
+	
+	printf("Last digit is not zero");
+	return 0;
+	
+}
