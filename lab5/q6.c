bd2c637fbf52b2b4f7f4f5674ffb9919501ebf5f
armeenazam <ayanazam1976@gmail.com>
2024-09-25 21:12:05 +0500

    Update q6.c

--- lab5/q6.c
+++ lab5/q6.c
@@ -6,4 +6,4 @@ int main()
     printf("Value of x:%d",x);
     return 0;
 }
-/*The output will be 30 because of operator precedence. Firstly, it checks first condition which is false so it move towards else statement. In else block it will check 2<5 first because according to operator precedence <,> comes before == and != ,since 2 < 5 is true so it gives 1 which is equal to 1 so this whole condition becomes and the output becomes 30
+/*The output will be 30 because of operator precedence. Firstly, it checks first condition which is false so it move towards else statement. In else block it will check 2<5 first because according to operator precedence <,> comes before == and != ,since 2 < 5 is true so it gives 1 which is equal to 1 so this whole condition becomes and the output becomes 30*/
